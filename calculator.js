e923199b49bd214d7c592784247b8735429bf6e2
Sarah Francis <sarah.katherinefrancis@gmail.com>
2023-05-02 11:55:34 +0200

    feat: Added functionality for performing arithmetic operations in the calculator

--- calculator.js
+++ calculator.js
@@ -1,5 +1,5 @@
-let buffer = "0";
 let runningTotal = 0;
+let buffer = "0";
 let previousOperator;
 const screen = document.querySelector(".screen");
 
@@ -12,35 +12,59 @@ function buttonClick(value) {
   rerender();
 }
 
-function handleNumber(number) {
+function handleNumber(value) {
   if (buffer === "0") {
-    buffer = number;
+    buffer = value;
   } else {
-    buffer += number;
+    buffer += value;
   }
 }
 
 function handleMath(value) {
   if (buffer === "0") {
+    // do nothing
     return;
   }
+
   const intBuffer = parseInt(buffer);
   if (runningTotal === 0) {
     runningTotal = intBuffer;
   } else {
     flushOperation(intBuffer);
   }
+
   previousOperator = value;
+
   buffer = "0";
 }
 
-function handleSymbol(symbol) {
-  switch (symbol) {
+function flushOperation(intBuffer) {
+  if (previousOperator === "+") {
+    runningTotal += intBuffer;
+  } else if (previousOperator === "-") {
+    runningTotal -= intBuffer;
+  } else if (previousOperator === "×") {
+    runningTotal *= intBuffer;
+  } else {
+    runningTotal /= intBuffer;
+  }
+}
+
+function handleSymbol(value) {
+  switch (value) {
     case "C":
       buffer = "0";
+      runningTotal = 0;
       break;
     case "=":
-      console.log("equals");
+      if (previousOperator === null) {
+        // need two numbers to do math
+        return;
+      }
+      flushOperation(parseInt(buffer));
+      previousOperator = null;
+      buffer = +runningTotal;
+      runningTotal = 0;
       break;
     case "←":
       if (buffer.length === 1) {
@@ -49,13 +73,17 @@ function handleSymbol(symbol) {
         buffer = buffer.substring(0, buffer.length - 1);
       }
       break;
-    case "÷":
-    case "×":
-    case "-":
     case "+":
-      handleMath(symbol);
+    case "-":
+    case "×":
+    case "÷":
+      handleMath(value);
       break;
   }
+}
+
+function rerender() {
+  screen.innerText = buffer;
 }
 
 function init() {
@@ -64,10 +92,6 @@ function init() {
     .addEventListener("click", function (event) {
       buttonClick(event.target.innerText);
     });
-}
-
-function rerender() {
-  screen.innerText = buffer;
 }
 
 init();
